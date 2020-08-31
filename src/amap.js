12bef45213c96340d837fb6c0ccb449abb8cbc9f
NicodeSS <RD_Nicode@outlook.com>
2020-08-31 14:08:32 +0800

    feat: add toolbar to map

--- src/amap.js
+++ src/amap.js
@@ -4,8 +4,13 @@ import privInfo from "./private";
 import { Map } from "react-amap";
 
 class MapComponent extends React.Component {
+  constructor() {
+    super();
+    const self = this;
+    this.mapPlugins = ["ToolBar"];
+  }
   render() {
-    return <Map amapkey={privInfo.AMAP_KEY} />;
+    return <Map amapkey={privInfo.AMAP_KEY} plugins={this.mapPlugins}></Map>;
   }
 }
 export default MapComponent;
