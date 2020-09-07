16a6e926e98e51ba8239dcd5a90c9995f0e78e37
NicodeSS <RD_Nicode@outlook.com>
2020-09-07 11:25:58 +0800

    feat: enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import "./App.css";
 
 import MapComponent from "./amap";
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <MapComponent />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <MapComponent />
+      </div>
+    </ThemeProvider>
   );
 }
 
