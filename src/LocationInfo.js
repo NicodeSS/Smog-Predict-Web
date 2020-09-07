64c639a95a89f94e5b52a8f90b310634523df53b
NicodeSS <RD_Nicode@outlook.com>
2020-09-07 16:32:43 +0800

    feat: add a AQI chart example

--- src/LocationInfo.js
+++ src/LocationInfo.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import { CardHeader } from "@material-ui/core";
+import { Card, CardHeader, CardContent } from "@material-ui/core";
+import ReactEcharts from "echarts-for-react";
+import axios from "./plugins/axios";
 
 const layerStyle = {
   position: "absolute",
+  minWidth: "400px",
   top: "10px",
   left: "10px",
 };
@@ -11,13 +13,12 @@ const layerStyle = {
 class LocationInfo extends React.Component {
   constructor(props) {
     super(props);
-    console.log("Props:", props);
     const self = this;
     const AMap = window.AMap;
     AMap.plugin("AMap.Geocoder", () => {
       self.geocoder = new AMap.Geocoder({ city: "全国" });
     });
-    this.state = { currentLocation: "Test", lng: 0, lat: 0 };
+    this.state = { currentLocation: "Test", lng: 0, lat: 0, options: {} };
   }
   componentDidMount() {
     this.props.onRef(this);
@@ -31,13 +32,21 @@ class LocationInfo extends React.Component {
           title={this.state.currentLocation}
           style={{ "background-color": "red" }}
         />
-        {this.state.lng},{this.state.lat}
+        <CardContent>
+          {this.state.lng},{this.state.lat}
+          <ReactEcharts
+            option={this.state.options}
+            style={{ height: "250px", width: "100%" }}
+            className="react_for_echarts"
+          />
+        </CardContent>
       </Card>
     );
   }
 
   async updateInfo() {
     let lnglat = [this.props.longitude, this.props.latitude];
+    this.getOptions();
     console.log(lnglat, typeof lnglat);
     this.geocoder &&
       (await this.geocoder.getAddress(lnglat, (status, result) => {
@@ -64,6 +73,157 @@ class LocationInfo extends React.Component {
           });
         }
       }));
+  }
+  async getOptions() {
+    try {
+      let result = await axios.get("test.json");
+      let data = result.data;
+      this.setState({
+        options: {
+          backgroundColor: "#ccc3",
+          textStyle: {
+            color: "#fff",
+          },
+          title: {
+            text: "Beijing AQI",
+            textStyle: {
+              color: "#fff",
+            },
+          },
+          tooltip: {
+            trigger: "axis",
+          },
+          xAxis: {
+            data: data.map(function (item) {
+              return item[0];
+            }),
+            axisLine: {
+              lineStyle: {
+                color: "#ccc",
+              },
+            },
+          },
+          yAxis: {
+            splitLine: {
+              show: false,
+            },
+            axisLine: {
+              lineStyle: {
+                color: "#ccc",
+              },
+            },
+          },
+          visualMap: {
+            top: 10,
+            right: 10,
+            show: false,
+            pieces: [
+              {
+                gt: 0,
+                lte: 50,
+                color: "#096",
+              },
+              {
+                gt: 50,
+                lte: 100,
+                color: "#ffde33",
+              },
+              {
+                gt: 100,
+                lte: 150,
+                color: "#ff9933",
+              },
+              {
+                gt: 150,
+                lte: 200,
+                color: "#cc0033",
+              },
+              {
+                gt: 200,
+                lte: 300,
+                color: "#660099",
+              },
+              {
+                gt: 300,
+                color: "#7e0023",
+              },
+            ],
+            outOfRange: {
+              color: "#999",
+            },
+          },
+          series: [
+            {
+              name: "历史数据",
+              type: "line",
+              showSymbol: false,
+              data: data.map(function (item, index) {
+                return index <= 24 ? item[1] : null;
+              }),
+              markLine: {
+                silent: true,
+                data: [
+                  {
+                    yAxis: 50,
+                  },
+                  {
+                    yAxis: 100,
+                  },
+                  {
+                    yAxis: 150,
+                  },
+                  {
+                    yAxis: 200,
+                  },
+                  {
+                    yAxis: 300,
+                  },
+                ],
+              },
+            },
+            {
+              name: "预测数据",
+              type: "line",
+              showSymbol: false,
+              smooth: false,
+              itemStyle: {
+                normal: {
+                  lineStyle: {
+                    width: 3,
+                    type: "dotted", //'dotted'虚线 'solid'实线
+                  },
+                },
+              },
+              data: data.map(function (item, index) {
+                return index >= 24 ? item[1] : null;
+              }),
+              markLine: {
+                silent: true,
+                data: [
+                  {
+                    yAxis: 50,
+                  },
+                  {
+                    yAxis: 100,
+                  },
+                  {
+                    yAxis: 150,
+                  },
+                  {
+                    yAxis: 200,
+                  },
+                  {
+                    yAxis: 300,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
---
+++ src/plugins/axios.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+let http = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://test.nicode.top:60005/"
+      : "http://localhost:3000/",
+  withCredentials: false,
+  timeout: 10000,
+});
+
+export default {
+  get: function (url, params) {
+    return new Promise((resolve, reject) => {
+      http({
+        method: "GET",
+        url: url,
+        params: params,
+      })
+        .then(function (res) {
+          !res.data.status ? resolve(res) : reject(res);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+    });
+  },
+  post: function (url, params) {
+    return new Promise((resolve, reject) => {
+      http({
+        method: "POST",
+        url: url,
+        data: params,
+      })
+        .then(function (res) {
+          !res.data.status ? resolve(res) : reject(res);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+    });
+  },
+  put: function (url, params) {
+    return new Promise((resolve, reject) => {
+      http({
+        method: "PUT",
+        url: url,
+        data: params,
+      })
+        .then(function (res) {
+          !res.data.status ? resolve(res) : reject(res);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+    });
+  },
+  delete: function (url, params) {
+    return new Promise((resolve, reject) => {
+      http({
+        method: "DELETE",
+        url: url,
+        params: params,
+      })
+        .then(function (res) {
+          !res.data.status ? resolve(res) : reject(res);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+    });
+  },
+};
