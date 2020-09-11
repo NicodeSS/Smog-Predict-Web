ab9394d3d93f17d98bc23cb70bb57e5589f2312f
NicodeSS <RD_Nicode@outlook.com>
2020-09-11 11:51:39 +0800

    feat: restrict to query Beijing information only

--- src/LocationInfo.js
+++ src/LocationInfo.js
@@ -484,20 +484,83 @@ class LocationInfo extends React.Component {
   async updateInfo(settings) {
     let lnglat = [this.props.longitude, this.props.latitude];
     let location = await this.getLocation(lnglat);
-    let airQuality = await this.getAirQuality(lnglat, settings);
+    let airQuality = null;
+    if (!location.currentLocation.includes("北京市")) {
+      settings.unsupported = true;
+      this.setState({ errMsg: "暂不支持北京以外地区", snackbar: true });
+    }
+    airQuality = await this.getAirQuality(lnglat, settings);
     let newState = {};
     Object.assign(newState, location, airQuality);
     this.setState(newState);
   }
   async getAirQuality(lnglat, settings) {
     try {
-      const { isTest } = settings;
-      let url = !isTest ? "/predict" : "/predict_test";
-      let result = await axios.get(url, {
-        longitude: lnglat[0],
-        latitude: lnglat[1],
-      });
-      let data = result.data.data;
+      const { isTest, unsupported } = settings;
+      let data = null;
+      if (!unsupported) {
+        let url = !isTest ? "/predict" : "/predict_test";
+        let result = await axios.get(url, {
+          longitude: lnglat[0],
+          latitude: lnglat[1],
+        });
+        data = result.data.data;
+        console.log(data);
+      } else {
+        data = {
+          "PM2.5": [
+            ["08:00", 0],
+            ["09:00", 0],
+            ["10:00", 0],
+            ["11:00", 0],
+            ["12:00", 0],
+            ["13:00", 0],
+            ["14:00", 0],
+            ["15:00", 0],
+            ["16:00", 0],
+            ["17:00", 0],
+            ["18:00", 0],
+            ["19:00", 0],
+            ["20:00", 0],
+            ["21:00", 0],
+            ["22:00", 0],
+            ["23:00", 0],
+            ["00:00", 0],
+            ["01:00", 0],
+            ["02:00", 0],
+            ["03:00", 0],
+            ["04:00", 0],
+            ["05:00", 0],
+            ["06:00", 0],
+            ["07:00", 0],
+            ["08:00", 0],
+            ["09:00", 0],
+            ["10:00", 0],
+            ["11:00", 0],
+            ["12:00", 0],
+            ["13:00", 0],
+            ["14:00", 0],
+            ["15:00", 0],
+            ["16:00", 0],
+            ["17:00", 0],
+            ["18:00", 0],
+            ["19:00", 0],
+            ["20:00", 0],
+            ["21:00", 0],
+          ],
+          airQuality: [],
+        };
+        for (let i = 0; i < 13; i++)
+          data.airQuality[i] = {
+            AQI: 0,
+            "PM2.5": 0,
+            PM10: 0,
+            NO2: 0,
+            CO: 0,
+            SO2: 0,
+            O3: 0,
+          };
+      }
       data.options = {
         backgroundColor: "#ccc3",
         textStyle: {
